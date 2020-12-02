6b355bbb40f199beaff30bb69d44ce7e80f62925
Sachith Ariyathilaka <sachith.vidu@yahoo.com>
2020-12-02 11:55:21 +0530

    Update AuthPreference.java

--- AuthPreference/authPrefLibrary/src/main/java/com/sachith/authpreflibrary/AuthPreference.java
+++ AuthPreference/authPrefLibrary/src/main/java/com/sachith/authpreflibrary/AuthPreference.java
@@ -6,38 +6,36 @@ import android.content.SharedPreferences;
 import java.util.ArrayList;
 
 public class AuthPreference {
-    public SharedPreferences sharedPreferences;
-    public SharedPreferences.Editor editor;
 
-    public void authUserdata(Context context, int userId, boolean state){
-        sharedPreferences = context.getSharedPreferences("User", Context.MODE_PRIVATE);
-        editor = sharedPreferences.edit();
+    public static void authUserdata(Context context, int userId, boolean state){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("User", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putInt("userId", userId);
         editor.putBoolean("status", state);
         editor.apply();
     }
 
-    public void saveUserHeaders(Context context, ArrayList<Headers> headers){
-        sharedPreferences = context.getSharedPreferences("User", Context.MODE_PRIVATE);
-        editor = sharedPreferences.edit();
+    public static void saveUserHeaders(Context context, ArrayList<Headers> headers){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("User", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         for(int i = 0; i < headers.size(); i++){
             editor.putString(headers.get(i).getHeader(), headers.get(i).getValue());
         }
         editor.apply();
     }
 
-    public Boolean getUserState(Context context){
-        sharedPreferences = context.getSharedPreferences("User",Context.MODE_PRIVATE);
+    public static Boolean getUserState(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("User",Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean("status", false);
     }
 
-    public int getUserId(Context context){
-        sharedPreferences = context.getSharedPreferences("User",Context.MODE_PRIVATE);
+    public static int getUserId(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("User",Context.MODE_PRIVATE);
         return sharedPreferences.getInt("userId", 0);
     }
 
-    public ArrayList<Headers> getUserHeaders(Context context, ArrayList<String> headerNames){
-        sharedPreferences = context.getSharedPreferences("User",Context.MODE_PRIVATE);
+    public static ArrayList<Headers> getUserHeaders(Context context, ArrayList<String> headerNames){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("User",Context.MODE_PRIVATE);
         ArrayList<Headers> headers = new ArrayList<>();
         for(int j =0 ; j<headerNames.size(); j++){
             headers.add(new Headers(headerNames.get(j), sharedPreferences.getString(headerNames.get(j), "")));
